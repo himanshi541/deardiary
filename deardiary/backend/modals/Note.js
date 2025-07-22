29e073578fd37a7dca55db40b12aefdd416a5fe8
himanshi541 <himanshijgoyal@gmail.com>
2025-07-22 16:48:45 +0530

    Add MongoDB BAckend with authentication and note routes
    Added Express server with JWT authentication, note CRUD operations, and middleware

--- deardiary/backend/modals/Note.js
+++ deardiary/backend/modals/Note.js
@@ -3,7 +3,7 @@ const { Schema } = mongoose;
 const NotesSchema = new Schema({
   user: { type: mongoose.Schema.Types.ObjectId, ref: "user" },
   title: { type: String, required: true },
-  Description: { type: String, required: true },
+  description: { type: String, required: true },
   tag: { type: String, default: "General" },
   date: { type: Date, default: Date.now },
 });
