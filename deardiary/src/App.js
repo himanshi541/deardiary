61c56a71b61ecc2b3c05e150ffcd0359f1c0dc74
himanshi541 <himanshijgoyal@gmail.com>
2025-07-24 21:29:18 +0530

    Router setup and pre requisites

--- deardiary/src/App.js
+++ deardiary/src/App.js
@@ -1,10 +1,20 @@
 import "./App.css";
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import Navbar from "./components/Navbar";
+import Home from "./components/home";
+import About from "./components/about";
 
 function App() {
   return (
-    <div className="App">
-      <h1>THIS IS DEARDIARY</h1>
-    </div>
+    <>
+      <Router>
+        <Navbar />
+        <Routes>
+          <Route exact path="/" element={<Home />} />
+          <Route exact path="/about" element={<About />} />
+        </Routes>
+      </Router>
+    </>
   );
 }
 
---
+++ deardiary/src/components/home.js
@@ -0,0 +1,7 @@
+import React from "react";
+
+const home = () => {
+  return <div>This is Dear Diary</div>;
+};
+
+export default home;
