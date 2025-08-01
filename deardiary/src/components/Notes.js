12d9d4b4c13bbdee7c984bb0fde6e16e388507fd
himanshi541 <himanshijgoyal@gmail.com>
2025-08-01 07:05:13 +0530

    Adding Delete Note

--- deardiary/src/components/Notes.js
+++ deardiary/src/components/Notes.js
@@ -1,19 +1,23 @@
 import React, { useContext } from "react";
 import noteContext from "../context/notes/noteContext";
 import NoteItem from "./NoteItem";
+import AddNote from "./AddNote";
 
 const Notes = () => {
   const context = useContext(noteContext);
-  const { notes, setnotes } = context;
+  const { notes, addNote } = context;
   return (
-    <div>
-      <div className="row my-3">
-        <h1>Your Thoughts</h1>
-        {notes.map((note) => {
-          return <NoteItem key={note._id} note={note} />;
-        })}
+    <>
+      <AddNote />
+      <div>
+        <div className="row my-3">
+          <h1>Your Thoughts</h1>
+          {notes.map((note) => {
+            return <NoteItem key={note._id} note={note} />;
+          })}
+        </div>
       </div>
-    </div>
+    </>
   );
 };
 
--- deardiary/src/context/notes/NoteState.js
+++ deardiary/src/context/notes/NoteState.js
@@ -1,4 +1,4 @@
-import react, { useState } from "react";
+import React, { useState } from "react";
 import NoteContext from "./noteContext";
 
 const NoteState = (props) => {
@@ -22,9 +22,33 @@ const NoteState = (props) => {
       __v: 0,
     },
   ];
-  const [notes, setnotes] = useState(notesInitial);
+  const [notes, setNotes] = useState(notesInitial);
+  //Add a note
+  const addNote = (title, description, tag) => {
+    const note = {
+      _id: "687f9b58308b757a865a8cb8",
+      user: "6849467a77bac63796c054df",
+      title: title,
+      description: description,
+      tag: tag,
+      date: "2025-07-22T14:08:24.256Z",
+      __v: 0,
+    };
+
+    setNotes(notes.concat(note));
+  };
+  //Del a Node
+  const delNode = (id) => {
+    console.log("Deleting a note with id" + id);
+    const newNotes = notes.filter((note) => {
+      return note._id !== id;
+    });
+    setNotes(newNotes);
+  };
+  //Edit a Note
+  const editNode = (id, title, description, tag) => {};
   return (
-    <NoteContext.Provider value={{ notes, setnotes }}>
+    <NoteContext.Provider value={{ notes, addNote, delNode, editNode }}>
       {props.children}
     </NoteContext.Provider>
   );
